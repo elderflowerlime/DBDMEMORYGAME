bab0c90f623be3df0a1b8d6f412bf32a4e312aa6
elderflowerlime <antczak.julia96@gmail.com>
2022-08-04 15:32:10 +0200

    base

---
+++ app.js
@@ -0,0 +1,52 @@
+const cardArray = [
+    {
+        name: "dwight",
+        img: "images/DWIGHT.png",
+    },
+    {
+        name: "jake",
+        img: "images/JAKE.png",
+    },
+    {
+        name: "yui",
+        img: "images/YUI.png",
+    },
+    {
+        name: "meg",
+        img: "images/MEG.png",
+    },
+    {
+        name: "claudette",
+        img: "images/CLAUDETTE.png",
+    },
+    {
+        name: "david",
+        img: "images/DAVID.png",
+    },
+    {
+        name: "dwight",
+        img: "images/DWIGHT.png",
+    },
+    {
+        name: "jake",
+        img: "images/JAKE.png",
+    },
+    {
+        name: "yui",
+        img: "images/YUI.png",
+    },
+    {
+        name: "meg",
+        img: "images/MEG.png",
+    },
+    {
+        name: "claudette",
+        img: "images/CLAUDETTE.png",
+    },
+    {
+        name: "david",
+        img: "images/DAVID.png",
+    }
+]
+
+console.log(cardArray);
